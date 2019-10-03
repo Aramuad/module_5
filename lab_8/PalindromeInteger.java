ee724bb0ac985fa6dbef180f6f25814ee470dbb6
Sam Ballard <samcballard@gmail.com>
2019-10-03 15:03:45 -0400

    polish

--- lab_8/PalindromeInteger.java
+++ lab_8/PalindromeInteger.java
@@ -3,25 +3,37 @@ import java.util.Scanner;
 public class PalindromeInteger {
 
 	public static void main(String[] args) {
-		output();
+		isPalindrome();
+	}	
+	
+	private static void isPalindrome() {
+		int[] arr = input();
+		int[] reversedArr = reverse(arr);
+		int number = 0;
+			for(int i = 0; i < reversedArr.length; i++) {
+				number = (number * 10) + reversedArr[i];
+			}
+		if (number == arr[0]) {
+			System.out.println();
+			System.out.println("Entered value:   " + number);
+			System.out.println("Judgment:        Palindrome");
+			System.out.println();
+		} else {
+			System.out.println();
+			System.out.println("Entered value:   " + arr[0]);
+			System.out.println("Judgement:       Not palindrome");
+			System.out.println();
+		}
 	}
+	
 	private static int[] input() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println();
-		System.out.print("Enter a number to check if its a palindrome: ");
+		System.out.print("Enter a number: ");
 		int x = sc.nextInt();
 		int[] arr = new int[] {x, count(x)};
 		sc.close();
 		return arr;
-	}
-	
-	private static int count(int x) {
-		int counter = 0;
-		while(x > 0) {
-			x = x / 10;
-			counter++;
-		}
-		return counter;
 	}
 	
 	private static int[] reverse(int x[]) {
@@ -36,23 +48,12 @@ public class PalindromeInteger {
 		return arr1;
 	}
 	
-	private static void output() {
-		int[] arr = input();
-		int[] holder = reverse(arr);
-		int number = 0;
-			for(int i = 0; i < holder.length; i++) {
-				number = (number * 10) + holder[i];
-			}
-		if (number == arr[0]) {
-			System.out.println();
-			System.out.println("Entered value:   " + number);
-			System.out.println("Judgment:        Palindrome");
-			System.out.println();
-		} else {
-			System.out.println();
-			System.out.println("Entered value:   " + number);
-			System.out.println("Judgement:       Not palindrome");
-			System.out.println();
+	private static int count(int x) {
+		int counter = 0;
+		while(x > 0) {
+			x = x / 10;
+			counter++;
 		}
+		return counter;
 	}
 }
